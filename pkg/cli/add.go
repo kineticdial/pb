106e9af9bd729bb23b1883c973f830a933bd88ce
Patrick Arthur Brown <pat@pab.io>
2017-02-06 22:37:55 +0000

    Allow Add to accept changed files

--- pkg/cli/add.go
+++ pkg/cli/add.go
@@ -4,15 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/Lead-SCM/pb/pkg/lib"
 )
 
+// Add stages a given file to the repository.
 func Add(path string) {
-	// TODO: Check if in repository.
-	// Check if file exists
 	f, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -32,17 +33,42 @@ func Add(path string) {
 		log.Fatal(err)
 	}
 
-	// Write blob's key in index
-	index, err := os.OpenFile("./.pb/index", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	index, err := os.Open("./.pb/index")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s := fmt.Sprintf("%s %s\n", path, b.Hash())
-	_, err = index.WriteString(s)
+	indexBuf := bytes.NewBuffer(nil)
+	_, err = io.Copy(indexBuf, index)
 	if err != nil {
-		log.Println("got here")
 		log.Fatal(err)
 	}
+	indexContents := string(indexBuf.Bytes())
 	index.Close()
+
+	lines := strings.Split(indexContents, "\n")
+	itemFound := false
+	newLines := make([]string, 0, len(lines))
+
+	for _, line := range lines {
+		tokens := strings.Split(line, " ")
+
+		if tokens[0] == path {
+			newLines = append(newLines, formatIndexLine(path, b))
+			itemFound = true
+		} else {
+			newLines = append(newLines, line)
+		}
+	}
+
+	if !itemFound {
+		newLines = append(newLines, formatIndexLine(path, b))
+	}
+
+	newContents := strings.TrimLeft(strings.Join(newLines, "\n"), "\n")
+	ioutil.WriteFile("./.pb/index", []byte(newContents), 0666)
+}
+
+func formatIndexLine(path string, b *lib.Blob) string {
+	return fmt.Sprintf("%s %s", path, b.Hash())
 }
--- pkg/cli/add_test.go
+++ pkg/cli/add_test.go
@@ -27,13 +27,81 @@ func TestAdd(t *testing.T) {
 	io.Copy(buf, f)
 	f.Close()
 
-	if string(buf.Bytes()) != "./test_file.txt 572c291421cd821a5e821e28766d0bdb719c379d\n" {
+	expected := "./test_file.txt 572c291421cd821a5e821e28766d0bdb719c379d"
+	if string(buf.Bytes()) != expected {
+		t.Logf("expected: %s\ngot: %s", expected, string(buf.Bytes()))
 		t.Fail()
 	}
 
 	b, _ := lib.GetBlob("572c291421cd821a5e821e28766d0bdb719c379d")
 
 	if b.Contents != "contents\n" {
+		t.Fail()
+	}
+
+	// Teardown
+	os.RemoveAll("./.pb")
+	os.Remove("./test_file.txt")
+}
+
+func TestAddAdditional(t *testing.T) {
+	// Setup
+	os.MkdirAll("./.pb/objects", 0777)
+	f, _ := os.Create("./.pb/index")
+	f.Close()
+	f, _ = os.Create("./test_file1.txt")
+	f.WriteString("contents1\n")
+	f.Close()
+	f, _ = os.Create("./test_file2.txt")
+	f.WriteString("contents2\n")
+	f.Close()
+
+	// Test
+	cli.Add("./test_file1.txt")
+	cli.Add("./test_file2.txt")
+
+	f, _ = os.Open("./.pb/index")
+	buf := bytes.NewBuffer(nil)
+	io.Copy(buf, f)
+	f.Close()
+
+	expected := "./test_file1.txt e53b356df5a76565a769e67ad656e581afc9d1b6\n./test_file2.txt b56fa959a159c3a3ee54824989711aab309805ba"
+	if string(buf.Bytes()) != expected {
+		t.Fail()
+	}
+
+	// Teardown
+	os.RemoveAll("./.pb")
+	os.Remove("./test_file1.txt")
+	os.Remove("./test_file2.txt")
+}
+
+func TestAddMutate(t *testing.T) {
+	// Setup
+	os.MkdirAll("./.pb/objects", 0777)
+	f, _ := os.Create("./.pb/index")
+	f.Close()
+	f, _ = os.Create("./test_file.txt")
+	f.WriteString("contents\n")
+	f.Close()
+
+	// Test
+	cli.Add("./test_file.txt")
+
+	f, _ = os.OpenFile("./test_file.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	f.WriteString("additional contents\n")
+	f.Close()
+
+	cli.Add("./test_file.txt")
+
+	f, _ = os.Open("./.pb/index")
+	buf := bytes.NewBuffer(nil)
+	io.Copy(buf, f)
+	f.Close()
+
+	expected := "./test_file.txt 5b2489e7bf4366347c0921dcef135c5870a19757"
+	if string(buf.Bytes()) != expected {
+		t.Logf("expected %s\ngot: %s", expected, string(buf.Bytes()))
 		t.Fail()
 	}
 
